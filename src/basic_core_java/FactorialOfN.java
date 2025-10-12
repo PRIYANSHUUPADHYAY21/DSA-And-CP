054ffbde493bf3d668c24943d74db3593fcfcd6c
PRIYANSHUUPADHYAY21 <priycool21@gmail.com>
2025-10-12 09:18:31 +0530

    First commit

---
+++ src/basic_core_java/FactorialOfN.java
@@ -0,0 +1,21 @@
+package basic_core_java;
+
+public class FactorialOfN {
+	
+	public static int fac(int n) {
+		if(n<=2) {
+			return n;
+		}
+		int ans = 1;
+		for(int i =2; i<=n; i++) {
+			ans *= i;
+		}
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(fac(6));
+
+	}
+
+}
---
+++ src/basic_core_java/PrimeNumber.java
@@ -0,0 +1,30 @@
+package basic_core_java;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+	
+	public static boolean primeOrNot(int n) {
+		if(n<2) {
+			return false;
+		}
+		else if(n==2) {
+			return true;
+		}
+		for(int i = 2; i*i<n; i++) {
+			if(n%i==0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		System.out.println(primeOrNot(n));
+
+	}
+
+}
